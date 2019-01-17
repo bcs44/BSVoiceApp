b25fc895262f04d99f52aa93d6f11ae99861497a
bcs44 <bruna_silva_05@hotmail.com>
2019-01-17 15:15:09 +0000

    DEV

---
+++ app/src/main/java/com/example/bruna/bsvoiceapp/Falar.java
@@ -0,0 +1,13 @@
+package com.example.bruna.bsvoiceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Falar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_falar);
+    }
+}
